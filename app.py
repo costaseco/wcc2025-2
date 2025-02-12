f9344f14d9c480f023834866db0f578040e39725
João Costa Seco <jrcs@fct.unl.pt>
2025-02-12 16:16:34 +0000

    Demo from class

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    data = get_weather()
+    return render_template('index.html', weather=data)
 
+def get_weather():
+    url = 'https://api.open-meteo.com/v1/forecast?latitude=38.67&longitude=-9.32&current_weather=true&hourly=precipitation,cloudcover,winddirection_10m,apparent_temperature,temperature_2m,relativehumidity_2m,windspeed_10m'
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+    return data['hourly']
 
 if __name__ == '__main__':
     app.run()
